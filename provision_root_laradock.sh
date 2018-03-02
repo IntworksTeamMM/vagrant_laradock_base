20742bad6595ed68eddbf9dc63f7b879061aae02
int_maruyama <int_maruyama@sakura.ad.jp>
2018-03-02 12:49:17 +0900

    [update] 間違い修正

--- provision_root_laradock.sh
+++ provision_root_laradock.sh
@@ -3,7 +3,7 @@
 # install my project
 cd /workspace
 
-if [-n ${MY_GIT_PROJECT_URL}]; then
+if [ -n ${MY_GIT_PROJECT_URL}]; then
  git clone ${MY_GIT_PROJECT_URL} ${PROJECT_NAME}
 fi
 
@@ -18,7 +18,7 @@ cp ./laradock/nginx/sites/laravel.conf.example ./laradock/nginx/sites/laravel.co
 sed -i "s/\(^MYSQL_DATABASE=\).*/\MYSQL_DATABASE=homestead/" ./laradock/.env
 sed -i "s/\(^MYSQL_USER=\).*/\MYSQL_USER=homestead/" ./laradock/.env
 sed -i "s/\(^MYSQL_PASSWORD=\).*/\MYSQL_PASSWORD=homestead/" ./laradock/.env
-if [-n ${PROJECT_NAME}]; then
+if [ -n ${PROJECT_NAME}]; then
     sed -i "s/\(^APPLICATION=\).*/\APPLICATION=..\/"${PROJECT_NAME}"\//" ./laradock/.env
-    sed -i "s:/var/www/laravel/public:/var/www/"${PROJECT_NAME}"/public:" ./laradock/nginx/sites/laravel.conf
+    sed -i "s:/var/www/laravel/public:/var/www/public:" ./laradock/nginx/sites/laravel.conf
 fi
